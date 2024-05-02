352cf421c407441494a11de0df9710b8af6d856c
Ilya M <ilyailiilia@gmail.com>
2024-05-02 07:34:14 +0200

    Pass args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from roles.job_description_extractor import JobDescriptionExtractor as extractor
 from roles.cv_form_filler import CvFormFiller as filler
 from roles.recruiter import Recruiter as recruiter
@@ -10,8 +11,13 @@ def make_cv(job_description_html: str, cv_template: str) -> str:
 
 
 if __name__ == "__main__":
-    job_description = open("resources/job_description.txt").read()
-    cv_template = open("resources/cv_template").read()
+    args = argparse.ArgumentParser()
+    args.add_argument("--job", type=str, required=True)
+    args.add_argument("--template", type=str, required=True)
+    args = args.parse_args()
+
+    job_description = open(args.job).read()
+    cv_template = open(args.template).read()
     review_passed = False
     while not review_passed:
         cv = make_cv(job_description, cv_template)
