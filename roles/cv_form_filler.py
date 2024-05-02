7e3741f33d35b27e9fceb8009465420f478767f0
Ilya M <ilyailiilia@gmail.com>
2024-05-02 10:25:13 +0200

    Prompt tuning

--- roles/cv_form_filler.py
+++ roles/cv_form_filler.py
@@ -7,9 +7,9 @@ class CvFormFiller:
 
     def fill(self, cv_template: str, job_description: str) -> str:
         cv = self._ollama.chat(
-            "Replace [skills] and [experience] with relevant skills and experience keeping them aligned with job titles in the following cv template: "
+            "Replace [skills] and [experience] with relevant skills and experience without repeating them in the following cv template: "
             + cv_template
-            + "\n to make a good match for the following job, avoid repeating description or already used phrases, make it short and informative, avoid using default and obvious things:\n"
+            + "\n to make a good match for the following job, make it short and informative, avoid using default and obvious things:\n"
             + job_description
         )
         return cv
