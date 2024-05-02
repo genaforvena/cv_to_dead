f2079273d2866866ab9e2707fd5274266d62fdab
Ilya M <ilyailiilia@gmail.com>
2024-05-02 20:51:15 +0200

    Simplify description before generation and try to get rid of duplicates if there are some.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 from roles.cv_form_filler import CvFormFiller as filler
+from roles.job_description_extractor import JobDescriptionExtractor as describer
+from roles.duplication_remover import ExperienceDuplicationRemover as filter
 
 
 if __name__ == "__main__":
@@ -14,6 +16,8 @@ if __name__ == "__main__":
         if not job_file.endswith(".txt"):
             continue
         job_description = open(os.path.join(args.jobs_folder, job_file)).read()
-        cv = filler().fill(cv_template, job_description)
+        abridged_job_description = describer().extract(job_description)
+        cv = filler().fill(cv_template, abridged_job_description)
+        cv_without_duplicates = filter().replace_duplicates_with_something_better(cv)
         cv_file = job_file.replace(".txt", "_cv.md")
         open(os.path.join(args.jobs_folder, cv_file), "w").write(cv)
--- roles/job_description_extractor.py
+++ roles/job_description_extractor.py
@@ -7,7 +7,7 @@ class JobDescriptionExtractor:
 
     def extract(self, job_description_html: str) -> str:
         job_description = self._ollama.chat(
-            "Extract in job description and key requirements for the role with description: "
+            "Extract in job description and key requirements for the role with description, squash skills that are similar and return only the key requirements: "
             + job_description_html
         )
         return job_description
